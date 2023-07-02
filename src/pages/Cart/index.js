d25d86b15d4ffccaee27fbfa9237f1d74c8c0479
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-07-02 20:24:24 -0300

    List cart items

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { CartContext } from '../../context/CartContext';
+import CartItem from '../../components/CartItem';
 
 export default function Cart() {
+  const { cart, addItemCart } = useContext(CartContext);
+
   return (
     <View style={styles.container}>
-      <Text>Página Carrinho</Text>
+      <FlatList
+        data={cart}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <CartItem data={item} addAmount={() => addItemCart(item)} />
+        )}
+      />
     </View>
   );
 }
@@ -12,5 +23,9 @@ export default function Cart() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FAFAFA',
+    paddingStart: 14,
+    paddingEnd: 14,
+    paddingTop: 14,
   },
 });
